91f534db588ecc1d5bd66af455aef03798477cff
fatasfaps <156978784+fatasfaps@users.noreply.github.com>
2026-01-05 12:24:58 +0000

    Deploying to gh-pages from @ fatasfaps/fatasfaps.github.io@99e29350905d37677ae3727638d4e2ed36e258e9 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "hasyafaps/hasyafaps.github.io",
+    "data-repo": "fatasfaps/fatasfaps.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
