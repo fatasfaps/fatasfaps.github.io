35b629ac468de89bad9f5f0e08f8ad9730ca4dcd
fatasfaps <156978784+fatasfaps@users.noreply.github.com>
2026-01-11 14:30:45 +0000

    Deploying to gh-pages from @ fatasfaps/fatasfaps.github.io@1b2b72f2bdd44e919658243d14d539396040e5e4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,14 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-bookshelf",
-          title: "bookshelf",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/books/";
-          },
-        },{id: "nav-projects",
+  },{id: "nav-projects",
           title: "Projects",
           description: "A growing collection of your cool projects.",
           section: "Navigation",
