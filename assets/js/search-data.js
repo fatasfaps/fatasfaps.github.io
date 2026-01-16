b4114bcb9f31e9f03e89189452e0ad20ca04e795
fatasfaps <156978784+fatasfaps@users.noreply.github.com>
2026-01-16 17:16:36 +0000

    Deploying to gh-pages from @ fatasfaps/fatasfaps.github.io@a8499c874e03cfec6a09ed5702c0cd54568c5388 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -474,39 +474,39 @@ ninja.data = [{
           section: "Scribbles",handler: () => {
               window.location.href = "/scribbles/2024-05-20-hari-ini/";
             },},{
-        id: 'social-cv',
-        title: 'CV',
-        section: 'Socials',
-        handler: () => {
-          window.open("/assets/pdf/example_pdf.pdf", "_blank");
-        },
-      },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%66%61%74%69%6D%61%68%61%73%79%61%70%6B%32%30%31%35@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-linkedin',
+        title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://www.linkedin.com/in/fatimahasya", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
+        id: 'social-github',
+        title: 'GitHub',
         section: 'Socials',
         handler: () => {
-          window.open("/feed.xml", "_blank");
+          window.open("https://github.com/fatasfaps", "_blank");
         },
       },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
+        id: 'social-medium',
+        title: 'Medium',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://medium.com/@fatasfaps", "_blank");
+        },
+      },{
+        id: 'social-goodreads',
+        title: 'Goodreads',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.goodreads.com/user/show/197331141-fatas", "_blank");
         },
       },{
         id: 'social-custom_social',
